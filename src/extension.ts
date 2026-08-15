47fdeb66680861d243901376bba7b18b140bca2a
Tim Revans <tim.revans@hotmail.co.uk>
2026-08-15 11:34:23 +0100

    Only show hover text for single line comments

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,15 @@ export function activate(context: vscode.ExtensionContext) {
 
     const hoverProvider = vscode.languages.registerHoverProvider('c', {
         provideHover(document, position, token) {
-            return new vscode.Hover('Hover Content');
+			const text = document.lineAt(position.line).text;
+			const commentStart = text.indexOf('//');
+			const currentChar = position.character;
+			const isComment = commentStart !== -1;
+			if (isComment && currentChar > commentStart) {
+				return new vscode.Hover('Hover Content');
+			}
+
+            return undefined;
         }
     });
 
