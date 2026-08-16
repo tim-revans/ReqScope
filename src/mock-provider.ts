9da034f76594e971ea1fc828bc7fb49ab5d7ad49
Tim Revans <timothy.revans@hotmail.co.uk>
2026-08-16 11:09:16 +0100

    Add Mock API provider

---
+++ src/mock-provider.ts
@@ -0,0 +1,18 @@
+import { RequirementProvider, RequirementData } from "./requirement-provider";
+
+export class mockProvider implements RequirementProvider {
+    name = 'Mock';
+    idPattern = /\[.*?\]/;
+
+    async fetchRequirement(id: string): Promise<RequirementData> {
+        return {
+            id: id,
+            title: "Requirement",
+            description: "Description.",
+            status: { message: "In Progress", color: "var(--vscode-testing-iconPassed)" },
+            assignee: "John Smith",
+            priority: { message: "High", color: "var(--vscode-errorForeground)" },
+            url: "https://www.google.com/",
+        };
+    }
+}
