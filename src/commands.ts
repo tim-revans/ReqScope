7e8e61a835c3b63c2428f45dcf89e88bf3a6aa1f
Tim Revans <timothy.revans@hotmail.co.uk>
2026-08-16 22:57:27 +0100

    chore: refactor commands

---
+++ src/commands.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+import { supportedTools } from './tools';
+
+export function registerCredentialCommands(context: vscode.ExtensionContext) {
+    for (const tool of supportedTools) {
+        if (tool.command && tool.setCredentials) {
+            const setCredentialsCmd = vscode.commands.registerCommand(tool.command, async () => {
+                const secrets = await tool.setCredentials!();
+                for (const secret of secrets) {
+                    await context.secrets.store(secret.name, secret.value);
+                }
+            });
+            context.subscriptions.push(setCredentialsCmd);
+        }
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,9 @@
 import * as vscode from 'vscode';
 import { getHoverProvider } from './hover';
-import { supportedTools } from './tools';
+import * as commands from './commands';
 
 export function activate(context: vscode.ExtensionContext) {
-	for (const tool of supportedTools) {
-		if (tool.command && tool.setCredentials) {
-			const setCredentialsCmd = vscode.commands.registerCommand(tool.command, async () => {
-				const secrets = await tool.setCredentials!();
-				for (const secret of secrets) {
-					await context.secrets.store(secret.name, secret.value);
-				}
-			});
-			context.subscriptions.push(setCredentialsCmd);
-		}
-	}
+	commands.registerCredentialCommands(context);
 
 	const hoverProvider = vscode.languages.registerHoverProvider('*', { provideHover: getHoverProvider(context) });
 	context.subscriptions.push(hoverProvider);
